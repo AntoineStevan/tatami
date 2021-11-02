cf15a76f6d635a6973cc228aec561c61bc6feac9
Antoine Stevan <stevan.antoine@gmail.com>
2021-11-02 14:42:31 +0100

    Basic argument parser.

--- tatami_solve.py
+++ tatami_solve.py
@@ -1,6 +1,17 @@
-def tatami_solve(xmax: int, ymax: int) -> list[facile.Solution]:
-        return facile.solve_all([], backtrack=True)
+import sys
+import facile
+
+
+def tatami_solve(xmax: int, ymax: int):# -> list[facile.Solution]:
+    return facile.solve_all([], backtrack=True)
 
 
 if __name__ == "__main__":
-    tatami_solve(6, 6)
+    if len(sys.argv[1:]) not in [0, 2]:
+        print("you should give exactly 2 or no integer arguments... Aborting!")
+        exit()
+
+    xmax, ymax = (4, 3) if len(sys.argv[1:]) == 0 else map(int, sys.argv[1:])
+
+    tatami_solve(xmax, ymax)
+
